d7b1b647cb7150bad2bdf68c13722fbdb981f2f3
Yer Yang <yyhmg_86@yahoo.com>
2023-05-15 23:12:53 -0700

    Displays Tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const form = document.querySelector("form");
+//   form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     buildToDo(event.target["new-task-description"].value);
+//     form.reset();
+//   });
+// });
+// function buildToDo(toDoList) {
+//   const ul = document.querySelector("#tasks");
+//   const btn = document.createElement("button");
+//   btn.addEventListener("click", handleDelete);
+//   btn.textContent = "x";
+//   ul.textContent = `${toDoList} `;
+//   console.log(ul);
+//   ul.appendChild(btn);
+// }
+// function handleDelete(event) {
+//   event.target.parentNode.remove();
+// }
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListeners();
 });
+
+function addingEventListeners() {
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit);
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  //console.log(event);
+  const task = event.target[0].value;
+  //console.log(task);
+  displayTask(task);
+}
+
+function displayTask(task) {
+  const taskUl = document.getElementById("tasks");
+ // console.log(taskUl);
+ const taskLi = document.createElement("li");
+ taskLi.textContent = task;
+ //console.log(taskLi);
+ taskUl.appendChild(taskLi);
+}
